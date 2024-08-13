c38a47d369a2192befb41a977129a948e5fe9c51
Rafael Barros <rafaelbarros.softwareengineer@gmail.com>
2024-08-12 23:46:22 -0300

    :construction: Build run Backend SCM

---
+++ backend-scm/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const github = require('./api/github');
+
+const app = express();
+const cors = require('cors');
+
+// Permitir CORS para a origem específica
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  }),
+);
+
+const apiPrefix = '/api/scm';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(`${apiPrefix}/github`, github);
+app.get(`${apiPrefix}/health`, (req, res) => res.json({ msg: 'Health OK' }));
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
